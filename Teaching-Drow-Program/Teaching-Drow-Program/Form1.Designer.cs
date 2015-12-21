a07fc6217aa9b00dba0248f4d8910b25eadb8d5c
regus2216 <sin.regus@gmail.com>
2015-12-22 01:25:45 +0900

    なんか線引けた

--- Teaching-Drow-Program/Teaching-Drow-Program/Form1.Designer.cs
+++ Teaching-Drow-Program/Teaching-Drow-Program/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picNote = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picNote)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(969, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picNote
+            // 
+            this.picNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picNote.Location = new System.Drawing.Point(0, 79);
+            this.picNote.Name = "picNote";
+            this.picNote.Size = new System.Drawing.Size(969, 420);
+            this.picNote.TabIndex = 1;
+            this.picNote.TabStop = false;
+            this.picNote.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picNote_MouseDown);
+            this.picNote.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picNote_MouseMove);
+            this.picNote.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picNote_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(969, 499);
+            this.Controls.Add(this.picNote);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "赤ペンソフト";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picNote)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picNote;
+
+       
     }
 }
 
--- Teaching-Drow-Program/Teaching-Drow-Program/Form1.cs
+++ Teaching-Drow-Program/Teaching-Drow-Program/Form1.cs
@@ -16,5 +16,62 @@ namespace Teaching_Drow_Program
         {
             InitializeComponent();
         }
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _bitmap = new Bitmap(picNote.Width, picNote.Height);//bitmapの初期化
+        }
+        
+
+        Bitmap _bitmap = null;//bitmapのインスタンス変数
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            using (Graphics g = picNote.CreateGraphics())//線とか色を描くのに必要
+            { 
+                g.DrawLine(Pens.Blue, 100, 100, 300, 300);
+            };
+             */
+            using (Graphics g = Graphics.FromImage(_bitmap)) {
+                g.DrawLine(Pens.Blue, 100, 100, 300, 300);
+            };
+            picNote.Image = _bitmap;//自動的に再描画
+        }
+
+        bool drawFlg = false;//true：描画中 
+
+        Point oldLocation = new Point();//線の終点を保存
+
+        private void picNote_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            //描画中でなければ　処理を抜ける
+            if (drawFlg == false) return;
+
+            using (Graphics g = Graphics.FromImage(_bitmap))
+            {
+                g.DrawLine(Pens.Black, oldLocation, e.Location);
+            };
+            picNote.Image = _bitmap;//自動的に再描画
+
+            oldLocation = e.Location;//新しい位置を保存
+        }
+
+        private void picNote_MouseUp(object sender, MouseEventArgs e)
+        {
+          //描画中を解除
+            drawFlg = false;
+        }
+
+        private void picNote_MouseDown(object sender, MouseEventArgs e)
+        {
+            oldLocation = e.Location;
+
+            //描画中にする
+            drawFlg = true;
+        }
+
+        
     }
 }
