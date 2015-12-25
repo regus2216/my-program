4a2e7aaeb5497762b626d9e15b1d07771a3915b3
regus2216 <sin.regus@gmail.com>
2015-12-25 09:40:28 +0900

    komachi ちょっと更新

--- komachi/komachi/komachi.c
+++ komachi/komachi/komachi.c
@@ -2,19 +2,23 @@
 小町算
 1.□1□2□3□4□5□6□7□8□9=100(□の中には"+","-","*","/ ",""のいずれかが入る
 2.順列通りにやるとすべての場合の数(131072通り)を総当りする
+
+
+仕様
+・符号を数字で表す。"+"->0,"-"->1,"*"->2,"/"->3,""->4
 */
 #include<stdio.h>
 #include<stdlib.h>
 
 int komachi[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-#define ANSWER 100
+#define ANSWER 100 /*小町算の解答*/
 
 const char *operator_str[] = { "+", "-"};
 
-/*小町算リスト*/
-struct rist_komach
-{
-	int nam; /*数値*/
-	int oper; /*演算子の種類*/
-};
+/*与えられた小町算の演算子配列で空白""をみつけ左に詰める*/
+void hyouka_Blank(int komachi,int hugou){
+
+
+	return 0;
+}
 
